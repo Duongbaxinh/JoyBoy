a289e81a7bea85c7900f640eaf5f3a5d767955c2
duongbaxinh <duongbaxinh2021@gmail.com>
2025-02-23 12:23:23 +0700

    update code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-red-300">Welcome to JoyBoy App</h1>
-    </div>
-  );
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <h1 className="text-red-300">Welcome to JoyBoy App update</h1>
+        </div>
+    );
 }
